6402970f5ad406f63f876314790c72a94326f837
S-JoBi23-053\student <231013@st.yoshida-g.ac.jp>
2024-06-14 10:17:12 +0900

    Player情報作成用のメソッドを作成 #3

--- ConsoleAppGE2/Player.cs
+++ ConsoleAppGE2/Player.cs
@@ -14,5 +14,55 @@ namespace ConsoleAppGE2
         string palyerName;
         int playerHp;
         int playerAtk;
+
+
+        /// <summary>
+        /// Player情報作成
+        /// </summary>
+        public void CreatePlayer(int playerNum)
+        {
+            Console.WriteLine("\n* [{0}番目] プレイヤー情報を追加します", playerNum);
+
+            // 名前を設定
+            Console.Write("1.Name > ");
+            this.palyerName = Console.ReadLine();
+
+            // HPを設定
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("2.HP > ");
+                bool isSuccess = int.TryParse(Console.ReadLine(), out this.playerHp);
+
+                if (isSuccess)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n*** ERROR ***\n");
+                }
+            }
+
+            // ATKを設定
+            while (true)
+            {
+                Console.Write("3.ATK > ");
+                bool isSuccess = int.TryParse(Console.ReadLine(), out this.playerAtk);
+
+                if (isSuccess)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n*** ERROR ***\n");
+                }
+            }
+
+            Console.Clear();
+        }
     }
 }
--- ConsoleAppGE2/Program.cs
+++ ConsoleAppGE2/Program.cs
@@ -8,8 +8,26 @@ namespace ConsoleAppGE2
 {
     internal class Program
     {
+        // プレイヤー人数
+        const int playerCntMax = 2;
+
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // newする
+            Player[] players = new Player[playerCntMax];
+
+            for(int i = 0; i < playerCntMax; i++)
+            {
+                // プレイヤー情報追加処理
+                players[i] = new Player();
+                players[i].CreatePlayer(i);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("##### ENTERで終了 #####");
+            Console.ReadLine();
         }
     }
 }
